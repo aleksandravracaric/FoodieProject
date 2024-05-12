5c45c124bb58de88f7aa4faeb5e82d4ef9930d8e
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-05-12 14:45:47 +0200

    centered header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import './style.css';
 
 export default function Header() {
     return (
-        <Navbar expand="lg" className=" bg-body-tertiary justify-content-center">
-            <Container className='containerLink'>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav className="two me-auto">
+        <Navbar expand="lg" className="bg-body-tertiary d-flex justify-content-center">
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className='justify-content-center'>
+                    <Nav>
                         <Nav.Link href="/">HOME</Nav.Link>
                         <Nav.Link href="/searchRecipes">SEARCH RECIPES</Nav.Link>
                         <Nav.Link href="/nutrition">NUTRITION</Nav.Link>
