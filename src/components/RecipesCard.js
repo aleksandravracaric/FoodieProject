56fa6062edfbf1ae10889cf20cbab7ec83a6f5cc
aleksandravracaric <aleksandravracaric997@gmail.com>
2024-08-05 22:25:41 +0200

    fixed card shadow

--- src/components/RecipesCard.js
+++ src/components/RecipesCard.js
@@ -1,6 +1,6 @@
 import { Card } from "react-bootstrap";
 import { ListGroup } from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './style.css'
 
 export default function RecipesCard(props) {
@@ -8,19 +8,20 @@ export default function RecipesCard(props) {
     const navigate = useNavigate();
 
     const toDetailPage = (recipe) => {
-        navigate('/recipe' , {state : {recipe}})
+        navigate('/recipe', { state: { recipe } })
     }
 
     return (
-              <Card style={{ width: '18rem' }} key={props.recipe.id} className="recipeCard" onClick={() => {toDetailPage(props.recipe)}} >
+        <Card style={{ width: '18rem' }} key={props.recipe.id} className="recipeCard" onClick={() => { toDetailPage(props.recipe) }} >
             <Card.Img variant="top" src={props.recipe.image} className="recipeImage" />
             <Card.Body>
                 <Card.Title className="recipeTitle">{props.recipe.title}</Card.Title>
             </Card.Body>
+            <hr />
             <ListGroup className="list-group-flush">
                 <ListGroup.Item className="recipeMinutes">{props.recipe.readyInMinutes}MIN   {props.recipe.servings} SERVES</ListGroup.Item>
             </ListGroup>
         </Card>
-      
+
     )
 }
